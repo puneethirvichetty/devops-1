a2ebb76020f793793a02ea6d51753363604e928a
Diksha Bajaj <47319191+Diksha86@users.noreply.github.com>
2019-10-14 12:25:29 +0000

    Create sonarqube.tf

---
+++ sonarqube.tf
@@ -0,0 +1,40 @@
+//SonarQube Instance
+resource "google_compute_address" "sonarqubeip" {
+  name   = "sonarqubeip"
+  region = "us-east1"
+}
+
+
+resource "google_compute_instance" "sonarqube" {
+  name         = "sonarqube-ashok-abc"
+  machine_type = "n1-standard-1"
+  zone         = "us-east1-b"
+
+  tags = ["name", "sonarqube", "http-server"]
+
+  boot_disk {
+    initialize_params {
+      image = "centos-7-v20180129"
+    }
+  }
+
+  // Local SSD disk
+  #scratch_disk {
+  #}
+
+  network_interface {
+    network    = "${var.sonarvpc}"
+    subnetwork = "${var.sonarsub)"
+
+
+    access_config {
+      // Ephemeral IP
+      nat_ip = "${google_compute_address.sonarqubeip.address}"
+    }
+  }
+  metadata = {
+    name = "sonarqube"
+  }
+
+  metadata_startup_script = "sudo yum update -y;sudo yum install git -y; sudo git clone https://github.com/iamdaaniyaal/devops.git; sudo chmod 777 /devops/*; sudo sh sonarqube.sh;"
+}
