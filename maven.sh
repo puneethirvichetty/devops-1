1ac5bb813a9c542494c08f1f4faf3d8c97c3be5f
Diksha Bajaj <47319191+Diksha86@users.noreply.github.com>
2019-10-14 14:34:11 +0000

    Update maven.sh

--- maven.sh
+++ maven.sh
@@ -5,6 +5,8 @@ sudo wget http://www-us.apache.org/dist/maven/maven-3/3.5.4/binaries/apache-mave
 sudo tar -xf apache-maven-3.5.4-bin.tar.gz
 sudo mv apache-maven-3.5.4/ apache-maven/
 cd /etc/profile.d/
-sudo vi maven.sh
+sudo vi maven1.sh
+sudo echo 'export M2_HOME=/usr/local/src/apache-maven' >> /etc/profile.d/maven1.sh
+sudo echo 'export PATH=${M2_HOME}/bin:${PATH}' >> /etc/profile.d/maven1.sh
 sudo chmod +x maven1.sh
-source /etc/profile.d/maven.sh
+source /etc/profile.d/maven1.sh
