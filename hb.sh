f463558d6d77f26f33b7c9355ef40673d0d74787
Diksha Bajaj <47319191+Diksha86@users.noreply.github.com>
2019-10-15 11:28:12 +0000

    Update hb.sh

--- hb.sh
+++ hb.sh
@@ -1,4 +1,4 @@
-sudo echo 'export harborip='$(curl -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)'' >> ~/.bash_profile
+echo 'export harborip=`curl -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip`' >> ~/.bash_profile
 source ~/.bash_profile
 sudo systemctl  stop firewalld && sudo systemctl  stop firewalld
 sudo curl -fsSL https://get.docker.com -o docker.sh
@@ -14,5 +14,5 @@ sudo yum update python*
 sudo docker-compose  -v
 sudo wget -P /opt/ https://storage.googleapis.com/harbor-releases/release-1.9.0/harbor-online-installer-v1.9.0.tgz
 sudo tar -xvf /opt/harbor-online*  -C /opt/
-sudo sed -i 's/hostname: reg.mydomain.com/hostname: $harborip/g'   /opt/harbor/harbor.yml
+sudo sed -i 's/hostname: reg.mydomain.com/hostname: '$harborip'/g'   /opt/harbor/harbor.yml
 sudo  sh  /opt/harbor/install.sh --with-clair
