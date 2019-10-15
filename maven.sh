ea27f6cf4856e8856bd26e9d10a8e9a4bf9c666f
Diksha Bajaj <47319191+Diksha86@users.noreply.github.com>
2019-10-15 05:51:51 +0000

    Update maven.sh

--- maven.sh
+++ maven.sh
@@ -1,13 +1,11 @@
 sudo yum install -y java-1.8.0-openjdk-devel
-sudo yum install wget -y
 cd /usr/local/src
-sudo wget http://www-us.apache.org/dist/maven/maven-3/3.5.4/binaries/apache-maven-3.5.4-bin.tar.gz
-sudo tar -xf apache-maven-3.5.4-bin.tar.gz
-sudo mv apache-maven-3.5.4/ apache-maven/
+sudo yum install -y maven
 cd /etc/profile.d/
-cat > maven1.sh << EOF
-export M2_HOME=/usr/local/src/apache-maven
+sudo touch maven1.sh
+sudo chmod 777 maven1.sh
+sudo cat > maven1.sh << EOF
+export M2_HOME=/usr/share/maven
 export PATH=${M2_HOME}/bin:${PATH}
 EOF
-sudo chmod +x maven1.sh
 source /etc/profile.d/maven1.sh
